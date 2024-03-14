766c385d27b1cdeed6d517b7ead20d445a30e2cc
Leonardo Monteiro Huck <leonardomonteirohuck@gmail.com>
2024-03-14 12:41:26 -0300

    Inclusao da classe Player

---
+++ Player.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace andar_e_pular_console
+{
+    internal class Player
+    {
+        public byte Posicao { get; set; }
+        public string Aparencia { get; set; }
+        public string FacingRight { get; set; }
+        public string FacingLeft { get; set; }
+        public Boolean Pulando { get; set; }  
+
+        public Player(string right, string left)
+        {
+            Posicao = 0;
+            FacingRight = right;
+            FacingLeft = left;
+            Aparencia = FacingRight;
+            Pulando = false;
+        }
+
+        public void Pula()
+        {
+            Console.Beep(234, 100);
+        }
+
+        public void anda(Boolean direcao, byte limite)
+        {
+            if (direcao)
+            {
+                if (Posicao < limite)
+                {
+                    Posicao++;
+                    Aparencia = FacingRight;
+                }
+            } 
+            else
+            {
+                if (Posicao > 0)
+                {
+                    Posicao--;
+                    Aparencia = FacingLeft;
+                }
+            }
+        }
+
+
+    }
+}
--- obj/Debug/net8.0/andar-e-pular-console.AssemblyInfo.cs
+++ obj/Debug/net8.0/andar-e-pular-console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("andar-e-pular-console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9222ee38e1a35c132e71118112f971b5180be80d")]
 [assembly: System.Reflection.AssemblyProductAttribute("andar-e-pular-console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("andar-e-pular-console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
